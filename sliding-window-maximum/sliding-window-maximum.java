6d5c7c559c207964727da43d7a6083274a223f0e
Vaibhav Agarwal <agarwalvaibhav041@gmail.com>
2022-01-03 16:19:39 +0530

    Time: 52 ms (29.52%), Space: 132.3 MB (10.94%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.java
+++ sliding-window-maximum/sliding-window-maximum.java
@@ -1,30 +1,55 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
-        int n=nums.length;
-        int nextGreater[]=new int[n];
-        Stack<Integer>st=new Stack<>();
-        Arrays.fill(nextGreater,n);
-        int i=0;
-        while(i<n)
-        {
+    //solutin using next greater element
+//     public int[] maxSlidingWindow(int[] nums, int k) {
+//         int n=nums.length;
+//         int nextGreater[]=new int[n];
+//         Stack<Integer>st=new Stack<>();
+//         Arrays.fill(nextGreater,n);
+//         int i=0;
+//         while(i<n)
+//         {
             
-            while(st.size()>0 && nums[st.peek()]<nums[i])
-            {
-                nextGreater[st.pop()]=i;
-            }
-                st.push(i++);
-        }
-        int ans[]=new int[n-k+1];
-        for( i=0;i<=n-k;i++)
-        {
-            int j=i;
-            while(nextGreater[j]<i+k)
-            {
-                j=nextGreater[j];
-            }
+//             while(st.size()>0 && nums[st.peek()]<nums[i])
+//             {
+//                 nextGreater[st.pop()]=i;
+//             }
+//                 st.push(i++);
+//         }
+//         int ans[]=new int[n-k+1];
+//         for( i=0;i<=n-k;i++)
+//         {
+//             int j=i;
+//             while(nextGreater[j]<i+k)
+//             {
+//                 j=nextGreater[j];
+//             }
             
-            ans[i]=nums[j];
-        }
-        return ans;
+//             ans[i]=nums[j];
+//         }
+//         return ans;
+//     }
+ public int[] maxSlidingWindow(int[] a, int k) {
+        int n = a.length;
+		int[] r = new int[n-k+1];
+		int ri = 0;
+		// store index
+		Deque<Integer> q = new ArrayDeque<>();
+		for (int i = 0; i < a.length; i++) {
+			// remove numbers out of range k
+			if (!q.isEmpty() && q.peek() == i - k) {
+				q.poll();
+			}
+			// remove smaller numbers in k range as they are useless
+			while (!q.isEmpty() && a[q.peekLast()] < a[i]) {
+				q.pollLast();
+			}
+            
+			q.offer(i);
+			if (i >= k - 1) {
+				r[ri++] = a[q.peek()];
+			}
+		}
+		return r;
     }
+        
 }
