c4e1a06dc7c80e2c52cea3f34fe6521ff19a4b04
Vaibhav Agarwal <agarwalvaibhav041@gmail.com>
2022-01-10 12:42:12 +0530

    Time: 9 ms (36.51%), Space: 44.8 MB (29.95%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList graph[]=new ArrayList[numCourses];
+        int cc[]=new int[numCourses];
+        for(int i=0;i<numCourses;i++)
+        {
+            graph[i]=new ArrayList<>();
+            
+        }
+        for(int pair[]:prerequisites)
+        {
+            cc[pair[1]]++;
+            graph[pair[0]].add(pair[1]);
+        }
+        int count=0;
+        Queue<Integer>q=new LinkedList<>();
+        for(int i=0;i<numCourses;i++)
+        {
+            if(cc[i]==0)q.add(i);
+        }
+        while(q.size()>0)
+        {
+            int rem=q.remove();
+            count++;
+            ArrayList<Integer>get=graph[rem];
+            for(int x:get)
+            {
+                cc[x]--;
+                if(cc[x]==0)
+                q.add(x);
+            }
+            
+        }
+        System.out.println(count);
+        return count>=numCourses;
+    }
+}
