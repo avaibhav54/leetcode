7c053ee3db06a889a83886c39240f012a995402b
Vaibhav Agarwal <agarwalvaibhav041@gmail.com>
2022-01-16 20:53:49 +0530

    Time: 6 ms (12.30%), Space: 46.4 MB (18.37%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
@@ -13,18 +13,14 @@ class Solution {
             if(arr[i]==1)break;
             right++;
         }
-        
-        
          int prev=-1;
         int till=0;
        for(int i=left;i<n-right;i++)
        {
            if(arr[i]==1)prev=i;
            till=Math.max(till,i-prev);
-           System.out.println(i+" "+prev+" "+till);
-           
        }
-        System.out.println(left+" "+right+" "+till);
+        
         int ans=Math.max(left,right);
         ans=Math.max(ans,till%2+till/2);
         return ans;
